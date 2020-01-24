758da7133d9b796a588986d491938198b4dcc8e3
Nikolay Monov <nmonov84@gmail.com>
2020-01-24 17:23:07 +0200

    added Footer,jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/common/Header';
+import Footer from './components/common/Footer';
 
 function App() {
   return (
-    <Header />
+    <div className='container'>
+      <Header />
+      <Footer />
+    </div>
   );
 }
 
