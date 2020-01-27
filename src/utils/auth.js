187fdbd4da71ddd030b7f711b1875b38286140a9
Nikolay Monov <nmonov84@gmail.com>
2020-01-27 09:42:59 +0200

    added auth requests

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,16 +1,38 @@
-import { APP_KEY, APP_SECRET, ADMIN_ROLE_ID } from './config'
-import { getCookieValue } from './cookiefunctions'
+import { BASE_URL, APP_KEY, USER_ROLE_ID, ADMIN_ROLE_ID } from './config'
+import { post, put } from './crud'
+import { setCookie, getCookieValue } from './cookiefunctions'
 
-const createCredentials = (type) => {
-    const basicCredentials = 'Basic ' + btoa(APP_KEY + ':' + APP_SECRET)
-    const kinveyCredentials = 'Kinvey ' + getCookieValue('authToken')
-    return (type === 'basic') ? basicCredentials : kinveyCredentials
-}
-
-const isLoggedIn = () => {
+function saveCookie (user) {
+    setCookie('username', user.username)
+    setCookie('authToken', user._kmd.authtoken)
+    setCookie('roleId', user.roleId)
+    setCookie('userId', user._id)
+  }
+  
+  function isAuth () {
     return getCookieValue('authToken') !== ''
+  }
+  
+  function isAdmin () {
+    return getCookieValue('roleId') === ADMIN_ROLE_ID
+  }
+
+function register ({ username, password, email, address }) {
+  const data = { username, password, email, address }
+  return post(`${BASE_URL}user/${APP_KEY}`, 'basic', data)
 }
 
-const isAdmin = () => {
-    return getCookieValue('roleId') === ADMIN_ROLE_ID
+async function login ({ username, password }) {
+  const data = { username, password }
+  return post(`${BASE_URL}user/${APP_KEY}/login`, 'basic', data)
 }
+
+async function assignRole (userId) {
+  return put(`${BASE_URL}user/${APP_KEY}/${userId}/roles/${USER_ROLE_ID}`, 'basic')
+}
+
+async function logout () {
+  return post(`${BASE_URL}user/${APP_KEY}/_logout`, 'kinvey')
+}
+
+export { register, login, assignRole, logout, saveCookie, isAuth, isAdmin }
---
+++ src/utils/crud.js
@@ -0,0 +1,30 @@
+import { APP_KEY, APP_SECRET } from './config'
+import { getCookieValue } from './cookiefunctions'
+
+function createCredentials (type) {
+  const basicCredentials = 'Basic ' + window.btoa(APP_KEY + ':' + APP_SECRET)
+  const kinveyCredentials = 'Kinvey ' + getCookieValue('authToken')
+  return (type === 'basic')
+    ? basicCredentials : kinveyCredentials
+}
+
+function request (method) {
+  return async function (url, credentials, data = {}, options = {}) {
+    const response = await window.fetch(url, {
+      method,
+      headers: {
+        'Authorization': createCredentials(credentials),
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: Object.keys(data).length ? JSON.stringify(data) : undefined,
+      ...options
+    })
+    return response.json()
+  }
+}
+
+export const get = request('get')
+export const post = request('post')
+export const put = request('put')
+export const remove = request('delete')
